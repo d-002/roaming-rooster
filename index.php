81abb50b5654c92c79ef5df8ad71a66178e289b4
D_00 <69427207+d-002@users.noreply.github.com>
2025-03-08 21:24:47 +0000

    added php.ini, test database, set up database with php

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<html>
-    <head>
-        <title>Hello world</title>
-    </head>
+<?php
+function getSecureDB() {
+    $db = new PDO("sqlite:test.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_PERSISTENT, true);
 
-    <body>
+    return $db;
+}
 
-        <?php
-// just a bit of PHP code to check if everything is working fine
-echo "<p>Hello, world!</p>";
-        ?>
+function sendSecureQuery($db, $query) {
+    try {
+        return null;
+    }
+    catch (Exception $e) {
+        echo "Failed: " . $e->getMessage();
+        return null;
+    }
+}
 
-    </body>
-</html>
+$db = getSecureDB();
+var_dump($db);
+
+$db = null; // close the connection (except no because it's persistent but it doesn't matter)
+?>
