7c0d6637fca7281a5aefb32399f6db771bad52fc
Alexis Fouquet <alexisfouquet05@gmail.com>
2025-03-15 20:06:13 +0000

    Starting register page

---
+++ components/header.php
@@ -0,0 +1,9 @@
+<?php function insertHeader($name) { ?>
+    <head>
+        <title>
+            <?php echo $name; ?>
+        </title>
+        <link rel="stylesheet" href="/assets/css/main_colors.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+<?php } ?>
---
+++ pages/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+?>
+<body>
+
+<div>
+    <header>
+        <h2>Join a strong community on</h2>
+        <h1>The Roaming Rooster</h1>
+    </header>
+
+    <div>
+        <div>
+            <div>
+                <img src="/assets/images/logo.png"
+            </div>
+        </div>
+        <div>
+            <div>
+                <div>
+                    I have an account
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
