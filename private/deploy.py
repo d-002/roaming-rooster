2590a87b8b712cbe02ded0ec6332783ed2c18e98
D_00 <69427207+d-002@users.noreply.github.com>
2025-03-15 00:35:27 +0000

    Added .github to auto-deploy ignore list

--- private/deploy.py
+++ private/deploy.py
@@ -6,7 +6,7 @@ from ftplib import FTP
 # ----- CONFIG -----
 
 HOME_DIR = '/htdocs'
-IGNORE_LIST = ['.git', '.gitignore', 'README.md']
+IGNORE_LIST = ['.git', '.github', '.gitignore', 'README.md']
 
 ignored = {key: False for key in IGNORE_LIST}
 
