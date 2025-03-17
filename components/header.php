2ee5d4457824f7c3562b1db9bd9a8b33fc345c70
Alexis Fouquet <alexisfouquet05@gmail.com>
2025-03-17 19:12:32 +0000

    Fixing variables' names

--- components/header.php
+++ components/header.php
@@ -4,7 +4,7 @@
         <title>
             <?php echo $name; ?>
         </title>
-        <link rel="stylesheet" href="/assets/css/main_colors.css">
+        <link rel="stylesheet" href="/assets/css/main_style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
               crossorigin="anonymous">
