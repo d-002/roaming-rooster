a7a715a48a5a77e32836d1f16a4cf6ba443b75b4
Alexis Fouquet <alexisfouquet05@gmail.com>
2025-03-19 13:23:48 +0000

    Small animation

--- assets/scripts/register.js
+++ assets/scripts/register.js
@@ -6,7 +6,9 @@ submit.addEventListener("click", e => {
     let removing = document.getElementsByClassName("remove-on-submit");
     console.log(removing.length)
     for (let mustBeRemoved of removing) {
-        mustBeRemoved.classList.add("hidden");
-        mustBeRemoved.remove();
+        mustBeRemoved.classList.add("reduce");
     }
+    let card = document.getElementsByClassName("card")[0];
+    card.classList.replace("card-half-page", "card-full-page");
+    let adding = document.getElementsByClassName("page-1");
 });
