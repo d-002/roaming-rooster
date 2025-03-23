fea6f8d4abbf3a41e9ff2187d2e2059dbc7115bd
D_00 <69427207+d-002@users.noreply.github.com>
2025-03-23 16:14:03 +0000

    Set up base for homepage, redirect

---
+++ pages/homepage/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    require $_SERVER["DOCUMENT_ROOT"] . "/utils/base.php";
+
+    component("header");
+    ?>
+
+    <body>
+        <h1>
+    </body>
+</html>
