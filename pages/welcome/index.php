2ceef27fd50b50c7b80f2aefde87e66730398c72
Alexis Fouquet <alexisfouquet05@gmail.com>
2025-03-23 17:41:28 +0000

    Insert in database

--- pages/welcome/index.php
+++ pages/welcome/index.php
@@ -4,14 +4,17 @@
 require $_SERVER["DOCUMENT_ROOT"] . "/utils/base.php";
 
 component("header");
+rootInclude("/private/db.php");
+rootInclude("/utils/dbutils.php");
 
 insertHeader("Welcome", ["inputs", "containers"]);
 ?>
 <body class="fill-page container align-content-center text-center">
 <?php
-if (isset($_REQUEST["display"])) {
+if (isset($_REQUEST["username"]) && isset($_REQUEST["password"]) && isset($_REQUEST["email"])) {
+    insertUserInDatabase(getSecureDB(), $_REQUEST["email"], $_REQUEST["username"], $_REQUEST["password"]);
     ?>
-    <p class="subtitle">Welcome <?php echo $_REQUEST["display"]; ?> on</p>
+    <p class="subtitle">Welcome <?php echo $_REQUEST["username"]; ?> on</p>
     <h1>The Roaming Rooster</h1>
     <a href="/pages/login"><p class="pointed">Login</p></a>
     <?php
--- utils/dbutils.php
+++ utils/dbutils.php
@@ -19,3 +19,29 @@ function isEmailInDatabase(PDO $db, $email): bool
     $st->closeCursor();
     return count($elements) != 0;
 }
+
+function insertUserInDatabase(PDO $db, $email, $username, $password, $display = null, $phone = null, $latitude = null, $longitude = null, $theme_id = null)
+{
+    $data = [
+        "email" => $email,
+        "username" => $username,
+        "password" => password_hash($password, PASSWORD_DEFAULT),
+        "display_name" => $display,
+        "phone" => $phone,
+        "verified_email" => 0,
+        "banned" => 0,
+        "theme_id" => $theme_id,
+        "latitude" => $latitude,
+        "longitude" => $longitude
+    ];
+
+    $data = array_filter($data, function ($value) {
+        return $value != null;
+    });
+
+    $columns = implode(", ", array_keys($data));
+    $values = implode(", :", array_keys($data));
+
+    $st = $db->prepare("INSERT INTO users ({$columns}) VALUES (:{$values})");
+    $st->execute($data);
+}
