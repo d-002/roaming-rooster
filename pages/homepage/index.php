bcc494f145d75ee6221a0c94f2f6f539c59b8b0e
D_00 <69427207+d-002@users.noreply.github.com>
2025-03-23 17:25:08 +0000

    Styled title, right image, buttons
    Addded light text color

--- pages/homepage/index.php
+++ pages/homepage/index.php
@@ -9,6 +9,32 @@
     ?>
 
     <body>
-        <h1>The Roaming Rooster</h1>
+        <div id="left">
+            <h2>Every local farmer is on</h2>
+            <h1>Roaming Rooster</h1>
+
+            <br class="large" />
+
+            <div class="center">
+            <?php
+function button($text, $link, $class) {
+    echo "<a href='".$link."' class='button ".$class."'>".$text."</a>";
+}
+
+if (false) {
+    button("Dashboard", "/pages/dashboard", "primary");
+    button("Sign out", "/pages/signout", "secondary");
+}
+else {
+    button("Sign up", "/pages/register", "primary");
+    button("Sign in", "/pages/login", "secondary");
+} ?>
+            </div>
+
+            <div id="arrow"></div>
+        </div>
+
+        <div id="right">
+        </div>
     </body>
 </html>
