42596896a86521a26c16357eb03f1b4f5187ebf0
D_00 <69427207+d-002@users.noreply.github.com>
2025-03-24 17:10:35 +0000

    Filling database

--- pages/admin/fill-database/index.php
+++ pages/admin/fill-database/index.php
@@ -82,7 +82,82 @@ function fillDatabase() {
     empty_database($db);
 
     // images
-    insert($db, "images", array("test"));
+    insert($db, "images", array("/assets/images/dummy/flower1.png"));
+    insert($db, "images", array("/assets/images/dummy/flower2.png"));
+    insert($db, "images", array("/assets/images/dummy/flower3.png"));
+    insert($db, "images", array("/assets/images/dummy/food1.png"));
+    insert($db, "images", array("/assets/images/dummy/food2.png"));
+    insert($db, "images", array("/assets/images/dummy/food3.png"));
+
+    insert($db, "themes", array(1, 2, 0xff0000, 0x00ff00));
+    insert($db, "themes", array(3, 4, 0xffffff, 0x0000ff));
+    insert($db, "themes", array(5, 6, 0x000000, 0x808080));
+
+    insert($db, "users", array("hello", "Hello", password_hash("hello", PASSWORD_DEFAULT), "hello@hello.com", 0, "+012345", .42, .69, 1, false));
+    insert($db, "users", array("world", "World", password_hash("world", PASSWORD_DEFAULT), "world@world.com", 1, "+056789", .69, .42, 2, false));
+    insert($db, "users", array("a", "a", password_hash("a", PASSWORD_DEFAULT), "a@a.com", 1, "+0", 3.14, 2.72, 0, false));
+    insert($db, "users", array("banned", "ImBanned", password_hash("banned", PASSWORD_DEFAULT), "im@banned.com", 1, "+11111", 0, 0, 3, true));
+
+    insert($db, "roles", array(1, 0));
+    insert($db, "roles", array(1, 1));
+    insert($db, "roles", array(2, 0));
+    insert($db, "roles", array(2, 2));
+    insert($db, "roles", array(3, 0));
+
+    insert($db, "balances", array(1, 42.69));
+    insert($db, "balances", array(2, 100));
+    insert($db, "balances", array(3, 0));
+    insert($db, "balances", array(3, 10));
+
+    insert($db, "conversations", array(1, 2, "Untitled", false));
+    insert($db, "conversations", array(1, 3, "Other", false));
+    insert($db, "conversations", array(3, 4, "Closed conversation", true));
+
+    insert($db, "notifications", array(1, 1, "notification for conversation 1", now()));
+    insert($db, "notifications", array(1, 2, "notification for conversation 2", now()));
+    insert($db, "notifications", array(2, 1, "notification for conversation 1 - other person", now()));
+    insert($db, "notifications", array(2, 2, "notification for conversation 2 - other person", now()));
+
+    insert($db, "messages", array(1, 1, "this is a message", now()-1000));
+    insert($db, "messages", array(2, 1, "hello", now()-500));
+    insert($db, "messages", array(1, 1, "world", now()));
+    insert($db, "messages", array(3, 2, "this is another message", now()-1000));
+    insert($db, "messages", array(4, 2, "hello", now()-500));
+    insert($db, "messages", array(3, 2, "world", now()-100));
+    insert($db, "messages", array(3, 2, "world2", now()));
+
+    insert($db, "conversations_requests", array(4, 3, false));
+    insert($db, "conversations_requests", array(3, 4, true));
+
+    insert($db, "tags", array("flower"));
+    insert($db, "tags", array("fruit"));
+    insert($db, "tags", array("vegetable"));
+    insert($db, "tags", array("gardening"));
+    insert($db, "tags", array("organic"));
+
+    insert($db, "tags_services_join", array(1, 1));
+    insert($db, "tags_services_join", array(2, 1));
+    insert($db, "tags_services_join", array(3, 1));
+    insert($db, "tags_services_join", array(4, 2));
+    insert($db, "tags_services_join", array(5, 3));
+
+    insert($db, "orders", array(1, 2, 1, 10));
+    insert($db, "orders", array(3, 4, 2, 20));
+    insert($db, "orders", array(1, 3, 3, 30));
+
+    insert($db, "ratings", array(1, 1, 3.5, "good"));
+    insert($db, "ratings", array(2, 2, 4.5, "very good"));
+    insert($db, "ratings", array(3, 3, 0, "very bad\nSecond line"));
+
+    insert($db, "services", array(1, 1, "selling flowers", "this service is selling flowers", .5, .5));
+    insert($db, "services", array(1, 1, "selling fruits", "this service is selling fruits", .8, .3));
+
+    insert($db, "sub_services", array(1, 10, "selling red flowers", "this subservice is selling red flowers", 2.72));
+    insert($db, "sub_services", array(1, 6, "selling yellow flowers", "this subservice is selling yellow flowers", 3.14));
+    insert($db, "sub_services", array(2, 42, "selling blue fruits", "this subservice is selling blue fruits", 6.9));
+
+    insert($db, "admin_logs", array(3, now(), "this is an admin log"));
+    insert($db, "admin_logs", array(3, now(), "this is another admin log"));
 
     $db = null;
 }
