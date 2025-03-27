369214e8e858e54bd030e2efc07d4f453de70459
Alexis Fouquet <alexisfouquet05@gmail.com>
2025-03-27 12:04:42 +0000

    Cancel when canceled

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,7 +1,9 @@
 let cancel = document.getElementById("cancel-search");
 let validate = document.getElementById("search-symbol");
+let search = document.getElementById("s");
 
 cancel.addEventListener("click", () => {
     cancel.classList.add("reduce-horizontal");
     validate.classList.remove("reduce-horizontal");
+    search.value = "";
 });
