a3fd0793d8f571e087f6d1303c7d84084d53a6c3
D_00 <69427207+d-002@users.noreply.github.com>
2025-04-06 23:32:10 +0100

    Added base for the chat system

---
+++ pages/chat/index.php
@@ -0,0 +1,30 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/utils/base.php";
+root_include("/utils/dbutils.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+component("header");
+insertHeader("Home", array("chat", array()));
+?>
+<body>
+    <div><div id="main">
+        <div class="mine"><p>This is my message</p></div>
+        <div class="other"><p>This is another's message</p></div>
+        <div class="mine"><p>This is my message</p></div>
+        <div class="other"><p>This is another's message</p></div>
+        <div class="mine"><p>This is my message</p><p>with a newline</p></div>
+        <div class="other"><p>This is another's message</p><p>with a newline</p></div>
+        <div class="mine"><p>This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message </p></div>
+        <div class="other"><p>This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message This is my very long message </p></div>
+    </div></div>
+
+    <div id="bottom">
+        <form method="POST">
+        </form>
+        <a id="refresh" alt="Refresh chat" href=""></a>
+    </div>
+</body>
+</html>
