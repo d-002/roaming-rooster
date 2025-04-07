8752eb2a4dd5e5ceb10d338d81035c6eb7ae6bbb
Alexis Fouquet <alexisfouquet05@gmail.com>
2025-04-07 23:44:33 +0100

    Remove notifications :star:

--- assets/scripts/notification.js
+++ assets/scripts/notification.js
@@ -1,10 +1,16 @@
-
 let notifications = document.getElementsByClassName("notification");
 for (const notification of notifications) {
     let remove = notification.querySelector(".remove-notification");
     if (remove) {
         remove.addEventListener("click", () => {
-
+            let request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (this.status === 200 && this.readyState === 4) {
+                    notification.remove();
+                }
+            }
+            request.open("POST", "/utils/notification.php");
+            request.send("id=" + notification.getAttribute("not"));
         });
     }
 }
