b40e1cd6d08b47cf01c3f149638d29e9d26d80a4
D_00 <69427207+d-002@users.noreply.github.com>
2025-04-10 16:19:05 +0100

    removed conflict tags in file, addressed changes

--- components/header.php
+++ components/header.php
@@ -7,28 +7,18 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
               integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
               crossorigin="anonymous">
-<<<<<<< HEAD
 
         <!-- stylesheets -->
         <link rel="stylesheet" href="/assets/css/main-style.css">
         <?php foreach($css as $href) {?>
             <link rel="stylesheet" href="/assets/css/<?=$href?>.css">
         <?php }?>
-=======
-        <?php
-        foreach ($css as $link) {
-            ?>
-            <link rel="stylesheet" href="/assets/css/<?= $link ?>.css">
-            <?php
-        }
-        if ($osm) {
-            ?>
+
+        <?php if ($osm) { ?>
+            <!-- openstreetmap -->
             <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
                   integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
                   crossorigin=""/>
-            <?php
-        }
-        ?>
->>>>>>> chat
+        <?php } ?>
     </head>
 <?php } ?>
